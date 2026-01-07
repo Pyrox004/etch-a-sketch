32a4f74487d852a11905e1a5171ef144485129fc
Pyrox004 <abderaghman.calendly@gmail.com>
2026-01-07 16:16:46 +0100

    feat: can draw in rainbow colors. has erase function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector(".container");
 const input = document.querySelector(".input-number");
 const resizerButton = document.querySelector(".resizer")
-const eraseButton = document.querySelector("erase")
+const eraseButton = document.querySelector(".erase")
+const rainbowButton = document.querySelector(".rainbow")
+const eraserButton = document.querySelector(".eraser")
+const blackButton = document.querySelector(".black")
 
 let isDrawing = false;
+let currentMode = "black"
 
 function gridGenerator(gridAmount) {
     const gridSize = gridAmount * gridAmount;
@@ -21,6 +25,8 @@ function gridGenerator(gridAmount) {
     }
 }
 
+gridGenerator(16)
+
 resizerButton.addEventListener("click", () => {
     let gridAmount = Number(input.value)
 
@@ -37,12 +43,43 @@ resizerButton.addEventListener("click", () => {
 
 eraseButton.addEventListener("click", () => {
     container.innerHTML = '';
+    let gridAmount = Number(input.value)
+
+    if (input.value == "" || input.value == null || input.value === undefined || input.value <=0) {
+        gridGenerator(16)
+    } else gridGenerator(gridAmount)
 })
+
+blackButton.addEventListener("click", () => {
+    currentMode = "black"
+})
+
+rainbowButton.addEventListener("click", () => {
+    currentMode = "rainbow"
+})
+
+eraserButton.addEventListener("click", () => {
+    currentMode = "erase"
+})
+
 
 container.addEventListener("mousedown", (e) => {
     isDrawing = true;
 
-    if (e.target.classList.contains("grid-square")) {
+    if (!isDrawing || !e.target.classList.contains("grid-square")) {
+        return
+    }
+
+    if (currentMode == "erase") {
+        e.target.style.backgroundColor = "white"
+        e.target.style.outline = "1px solid gray"
+    } else if (currentMode == "rainbow") {
+        const r = Math.floor(Math.random() * 256)
+        const g = Math.floor(Math.random() * 256)
+        const b = Math.floor(Math.random() * 256)
+
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    } else {
         e.target.style.backgroundColor = "black"
     }
 
@@ -53,8 +90,21 @@ window.addEventListener("mouseup", () => {
 })
 
 container.addEventListener("mouseover", (e) => {
-    if(isDrawing&& e.target.classList.contains("grid-square")) {
+    if (!isDrawing || !e.target.classList.contains("grid-square")) {
+        return
+    }
+
+    if (currentMode == "erase") {
+        e.target.style.backgroundColor = "white"
+    } else if (currentMode == "rainbow") {
+        const r = Math.floor(Math.random() * 256)
+        const g = Math.floor(Math.random() * 256)
+        const b = Math.floor(Math.random() * 256)
+
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    } else {
         e.target.style.backgroundColor = "black"
+        e.target.style.outline = "1px solid gray"
     }
 })
 
